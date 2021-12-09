941b4f2675fde07a6e4cfaad890cc95907d4bff6
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2021-12-08 17:03:40 -0700

    create app University for peer review and edits
    * Note this template has been taken from existing crypto projects and will need modification by community for Sumcoin continuity.

---
+++ app/herodotus-settings.js
@@ -0,0 +1,12 @@
+export default {
+  documentation_name: 'Sumcoin University',
+
+  allow_topbar_link: true,
+  topbar_link_label: 'Sumcoin University',
+  topbar_link_url: 'https://github.com/sumcoinlabs/SumcoinUniversity',
+
+  allow_font_size_control: true,
+
+  allow_remote_documentation: false,
+  remote_documentation_url: ''
+}
